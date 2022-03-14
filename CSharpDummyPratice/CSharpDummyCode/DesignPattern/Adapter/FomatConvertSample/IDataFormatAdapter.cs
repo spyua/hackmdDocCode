5bde9660479aa71122b2b89472703295c3ab5dec
pal2097 <pal2097@gmail.com>
2022-03-14 22:13:27 +0800

    feat: 🎸 Adapter Pattern Sample
    練習寫Adapter應用

---
+++ CSharpDummyPratice/CSharpDummyCode/DesignPattern/Adapter/FomatConvertSample/IDataFormatAdapter.cs
@@ -0,0 +1,9 @@
+﻿using DesignPattern.Adapter.FomatConvertSample.FormatModel;
+
+namespace DesignPattern.Adapter.FomatConvertSample
+{
+    public interface IDataFormatAdapter
+    {
+        object Convert();
+    }
+}
---
+++ CSharpDummyPratice/CSharpDummyCode/DesignPattern/Adapter/FomatConvertSample/JsonToXmlAdapter .cs
@@ -0,0 +1,21 @@
+﻿using DesignPattern.Adapter.FomatConvertSample.FormatModel;
+
+namespace DesignPattern.Adapter.FomatConvertSample
+{
+    public class JsonToXmlAdapter : IDataFormatAdapter
+    {
+        // 被轉接對象(Adaptee)
+        private Json json;
+        public JsonToXmlAdapter(Json json)
+        {
+            this.json = json;
+        }
+
+        public object Convert()
+        {
+            var xml = new Xml();
+            //  Logic to convert Json to Xml -> json.jsonStr
+            return xml;
+        }
+    }
+}
